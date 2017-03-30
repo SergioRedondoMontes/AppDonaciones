2bbfa71c764994d68adbea97f20c16ef85fec637
Ramsés Cayetano Diez Galvan <ramses.diez@live.u-tad.com>
2017-03-30 18:42:35 +0200

    MockUp registro

---
+++ Mockup_Bloo/Mockup_Bloo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mockup_Bloo
+//
+//  Created by Ramsés Cayetano Díez Galván on 30/3/17.
+//  Copyright © 2017 Ramsés Cayetano Díez Galván. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
