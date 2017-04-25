de20a1ea58692fc817282aacf6943441b061efd1
Sergio Redondo Montes <sergio.redondo@live.u-tad.com>
2017-04-25 12:15:27 +0200

    Parte visual 1.0

---
+++ bloo/bloo/VCInbox.swift
@@ -0,0 +1,35 @@
+//
+//  VCInbox.swift
+//  bloo
+//
+//  Created by Sergio Redondo on 25/4/17.
+//  Copyright © 2017 AppBloo. All rights reserved.
+//
+
+import UIKit
+
+class VCInbox: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- bloo/bloo/VCLogIn.swift
+++ bloo/bloo/VCLogIn.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class VCLogIn: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
